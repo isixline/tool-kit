0368afda6ceddbdfce837cff96e49600aeb59eb4
isixline <isixline@qq.com>
2023-08-06 17:31:41 +0800

    feat: add return prompt

--- summary/web-base-summarizer.py
+++ summary/web-base-summarizer.py
@@ -1,4 +1,5 @@
 import argparse
+from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
 from langchain.chains.summarize import load_summarize_chain
@@ -11,9 +12,14 @@ def summarize_web_page(url):
     docs = loader.load()
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
-    chain = load_summarize_chain(llm, chain_type="map_reduce")
 
-    summarize = chain.run(docs)
+    prompt_template = """Write a concise summary of the following:\n{text}\nCONCISE SUMMARY IN CHINESE:"""
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+    # chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+
+    chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT)
+
+    summarize = chain(docs, return_only_outputs=True)
     return summarize
 
 if __name__ == "__main__":
