bc1d237ae088ff4b4e7722a01aad0e609706b03a
isixline <isixline@qq.com>
2023-08-06 16:22:11 +0800

    feat: add web-base-summarizer.py

---
+++ summary/web-base-summarizer.py
@@ -0,0 +1,25 @@
+import argparse
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.chains.summarize import load_summarize_chain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def summarize_web_page(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+    chain = load_summarize_chain(llm, chain_type="stuff")
+
+    summarize = chain.run(docs)
+    return summarize
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", type=str)
+    args = parser.parse_args()
+
+    summary = summarize_web_page(args.url)
+    print(summary)
